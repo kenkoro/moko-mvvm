d6e6533d3d385eb46dbec93f6fc4b8d5ce1c82e9
kenkoro <frogharvard@gmail.com>
2025-09-16 09:11:54 +0700

    Replaced a deprecated usage of the `launchWhenStarted` property

--- mvvm-flow/src/androidMain/kotlin/dev/icerock/moko/mvvm/flow/binding/BindingBase.kt
+++ mvvm-flow/src/androidMain/kotlin/dev/icerock/moko/mvvm/flow/binding/BindingBase.kt
@@ -4,21 +4,26 @@
 
 package dev.icerock.moko.mvvm.flow.binding
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.DisposableHandle
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 fun <T> StateFlow<T>.bind(
     lifecycleOwner: LifecycleOwner,
     observer: (T) -> Unit
 ): DisposableHandle {
     val self: StateFlow<T> = this
-    val job: Job = lifecycleOwner.lifecycleScope.launchWhenStarted {
-        self.onEach { observer(it) }.collect()
+    val job: Job = lifecycleOwner.lifecycleScope.launch {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            self.onEach { observer(it) }.collect()
+        }
     }
     return DisposableHandle { job.cancel() }
 }
